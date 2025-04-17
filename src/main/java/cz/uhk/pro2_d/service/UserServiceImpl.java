fb816f5fbc3a9c344b6f8807b0370372f7dd75b7
pallado1 <dominik.palla@uhk.cz>
2025-04-17 13:30:56 +0200

    Security module

--- src/main/java/cz/uhk/pro2_d/service/UserServiceImpl.java
+++ src/main/java/cz/uhk/pro2_d/service/UserServiceImpl.java
@@ -2,7 +2,10 @@ package cz.uhk.pro2_d.service;
 
 import cz.uhk.pro2_d.model.User;
 import cz.uhk.pro2_d.repository.UserRepository;
+import cz.uhk.pro2_d.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -23,6 +26,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(User user) {
+        //Provést hashování hesla
         userRepository.save(user);
     }
 
@@ -34,5 +38,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteUser(long id) {
         userRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new MyUserDetails(user);
     }
 }
